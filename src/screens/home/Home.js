57088eebb51499ca7f229b2430a4e513b5294ed5
adi1729 <adiyadav.1729@gmail.com>
2021-02-21 23:14:09 +0530

    Added 'Release Start Date'

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
-import {withStyles} from '@material-ui/core/styles'; 
-import moviesData from '../../common/movieData'
+import { withStyles } from '@material-ui/core/styles';
+import moviesData from '../../common/movieData';
+import genres from '../../common/genres';
+import artists from '../../common/artists';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent' ;
-import FormControl from '@material-ui/core/FormControl' ;
-import Typography from '@material-ui/core/Typography' ;
-import InputLabel from '@material-ui/core/InputLabel' ;
-import Input from '@material-ui/core/Input' ;
-
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem'
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField'
 
 const styles = theme => ({
     root: {
-        flexGrow:1,
+        flexGrow: 1,
         backgroundColor: theme.palette.background.paper
     },
     upcomingMoviesHeading: {
         textAlign: 'center',
         background: '#ff9999',
-        padding : '8px',
-        fontSize : '1rem'
+        padding: '8px',
+        fontSize: '1rem'
     },
     gridListUpcomingMovies: {
         flexWrap: 'nowrap',
@@ -38,33 +44,44 @@ const styles = theme => ({
         margin: theme.spacing.unit,
         minWidth: 240,
         maxWidth: 240
-     },
-     title: {
+    },
+    title: {
         color: theme.palette.primary.light,
-     }
+    }
 });
 
 
-class Home extends Component{
+class Home extends Component {
 
-    constructor(){
+    constructor() {
         super();
-        this.state ={
-            movieName : ""
+        this.state = {
+            movieName: "",
+            genres: [],
+            artists: []
         }
     }
 
     movieNameChangeHandler = event => {
-        this.setState({movieName: event.target.value});
+        this.setState({ movieName: event.target.value });
 
     }
 
-    render(){
+    artistSelectHandler = event => {
+        this.setState({ artists: event.target.value });
+
+    }
+    
+    genreSelectHandler = event =>{
+        this.setState({genres : event.target.value})
+    }
+
+    render() {
         const { classes } = this.props;
         return (
             <div>
                 <Header />
-                <div className ={classes.upcomingMoviesHeading}>
+                <div className={classes.upcomingMoviesHeading}>
                     <span>
                         Upcoming Movies
                     </span>
@@ -95,23 +112,73 @@ class Home extends Component{
                     <div className="right">
                         <Card>
                             <CardContent>
-                                <FormControl className = {classes.formControl}>
-                                    <Typography className ={classes.title} color ="textSecondary">
+                                <FormControl className={classes.formControl}>
+                                    <Typography className={classes.title} color="textSecondary">
                                         FIND MOVIES BY:
-
                                     </Typography>
                                 </FormControl>
-
-                                <FormControl className = {classes.formControl} >
+                                <FormControl className={classes.formControl} >
                                     <InputLabel htmlFor="movieName"> Movie Name</InputLabel>
-                                    <Input id = "movieName" onChange={this.movieNameChangeHandler} />
+                                    <Input id="movieName" onChange={this.movieNameChangeHandler} />
                                 </FormControl>
+                                <FormControl className={classes.formControl} >
+                                    <InputLabel htmlFor="select-multiple-checkbox"> Genre</InputLabel>
+                                    <Select
+                                        multiple
+                                        input={<Input id="select-multiple-checkbox" />}
+                                        renderValue={selected => selected.join(',')}
+                                        value={this.state.genres}
+                                        onChange={this.genreSelectHandler}>
+                                        <MenuItem value="0"> None
+                                    </MenuItem>
+
+                                        {genres.map(genre => (
+                                            <MenuItem key={genre.id} value={genre.name} >
+                                                <Checkbox checked={this.state.genres.indexOf(genre.name) > -1} />
+                                                <ListItemText primary={genre.name} />
+                                            </ MenuItem>
+                                        ))}
+                                        </Select>
+                                        
+                                </FormControl>
+
+                                <FormControl className={classes.formControl} >
+                                    <InputLabel htmlFor="select-multiple-checkbox"> Artist</InputLabel>
+                                    <Select
+                                        multiple
+                                        input={<Input id="select-multiple-checkbox" />}
+                                        renderValue={selected => selected.join(',')}
+                                        value={this.state.artists}
+                                        onChange={this.artistSelectHandler}>
+                                        <MenuItem value="0"> None
+                                    </MenuItem>
+
+                                        {artists.map(artist => (
+                                            <MenuItem key={artist.id} value={artist.first_name +" "+artist.last_name} >
+                                                <Checkbox checked={this.state.artists.indexOf(artist.first_name +" "+ artist.last_name) > -1} />
+                                                <ListItemText primary={artist.first_name+ " "+artist.last_name} />
+                                            </ MenuItem>
+                                        ))}
+                                        </Select>
+                                        
+                                </FormControl>
+
+                                <FormControl className={classes.formControl} >
+                                    <TextField 
+                                    id ="releaseStartDate"
+                                    label ="Release Start Date"
+                                    type ="date"
+                                    defaultValue =""
+                                    InputLabelProps = {{shrink : true}}/>
+                                    </FormControl>
+                                
+
                             </CardContent>
                         </Card>
 
                     </div>
+                    </div>
                 </div>
-            </div>
         )
     }
 }
